a6693eefc192a1096e9d63f448fe720156f9b531
rgonz522 <rgonz522@fiu.edu>
2020-07-02 17:24:19 -0700

    added user's profile picture, screen name, and user name to the compose fragment

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeFragment.java
@@ -17,9 +17,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
@@ -44,6 +46,9 @@ public class ComposeFragment extends DialogFragment {
     EditText etCompose;
     Button btTweet;
     TextView tvCounter;
+    TextView tvScreenname;
+    TextView tvrealname;
+    ImageView ivProfilePic;
 
 
     TwitterClient client;
@@ -79,10 +84,8 @@ public class ComposeFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
 
 
-        }
 
 
 
@@ -98,10 +101,18 @@ public class ComposeFragment extends DialogFragment {
 
         View view = inflater.inflate(R.layout.fragment_compose, container, false);
 
+        tvScreenname = view.findViewById(R.id.tvScreenName);
+        tvrealname = view.findViewById(R.id.tvRealName);
+        ivProfilePic = view.findViewById(R.id.tvProfilePic);
 
         btTweet = view.findViewById(R.id.btTweet);
         etCompose = view.findViewById(R.id.etCompose);
         tvCounter = view.findViewById(R.id.tvCounter);
+
+        tvScreenname.setText("@" + TwitterClient.Current_User.screen_name);
+        tvrealname.setText(TwitterClient.Current_User.name);
+        Glide.with(this).load(TwitterClient.Current_User.profile_img_url).into(ivProfilePic);
+
         //set click listener on button
         //make an API Call to twitter
         btTweet.setOnClickListener(new View.OnClickListener()
